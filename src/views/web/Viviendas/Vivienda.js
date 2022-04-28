97c2e24a62d6e3af6c12dbf48fb0605c88bf8a6c
antoniopalomequeprogrammer <palomequework@gmail.com>
2022-04-29 01:24:57 +0200

    Vista vivienda editada

--- src/views/web/Viviendas/Vivienda.js
+++ src/views/web/Viviendas/Vivienda.js
@@ -7,6 +7,16 @@ import { toast } from "react-toastify";
 import { Button } from "@material-ui/core";
 import EmailIcon from '@material-ui/icons/Email';
 
+// display: flex;
+//     background: cadetblue;
+//     padding: 10px;
+//     margin-top: 20px;
+//     font-weight: 700;
+//     color: white;
+
+
+
+
 const Vivienda = (props) => {
     const [vivienda, setVivienda] = useState({});
     const id = props.match.params.id;
@@ -28,34 +38,80 @@ const Vivienda = (props) => {
 
     }
 
-
     return (
-       <GridContainer xs={12} sm={12} md={12} lg={12} xl={12} style={{display:"flex", justifyContent:"space-between",margin:"10px"}}>
-         <GridItem xs={10} sm={10} md={10} lg={10} xl={10} style={{height:"550px", border:"2px solid", display:"flex"}}>
-           <GridItem xs={8} sm={8} md={8} lg={8} xl={8} style={{width:"auto"}}>
-           <img style={{marginTop:"20px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
-           </GridItem>
+      <GridContainer xs={12} sm={12} md={12} lg={12} xl={12}>
+        {/* TÍTULO */}
+        <GridItem xs={12} sm={12} md={12} lg={12} xl={12}>
+          <h1>{vivienda.titulo}</h1>
+        </GridItem>
 
-           <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>
-            <h2>{vivienda.titulo}</h2>
-            <p>{vivienda.descripcion}</p>
-            <h3 style={{textAlign:"right",fontFamily:"fantasy"}}>{vivienda.precio}€</h3>
-            <div width="100%" style={{display:"flex", justifyContent:"space-between"}}>
-            <Button width="50%" variant="contained" color="primary">
-  Email
-</Button>
-            <Button width="50%" variant="contained" color="primary">
-  Llamar
-</Button>
-            </div>
-           </GridItem>
-           </GridItem>
-         <GridItem xs={2} sm={2} md={2} lg={2} xl={2} style={{height:"550px", border:"2px solid",padding:"0"}}>
-           <img width={"50px"} height={"50px"} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"  />
-           <h4 style={{textAlign:"center"}}>{vivienda.inmobiliaria}</h4>
-         </GridItem>
-       </GridContainer>
+        {/* IMAGENES */}
+        <GridItem xs={12} sm={12} md={12} lg={12} xl={12} style={{textAlign:"center"}}>
+          <GridItem xs={8} sm={8} md={8} lg={8} xl={8}>
+          <img style={{marginTop:"20px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+          </GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4} style={{display:"flex"}}>
+          <img style={{marginTop:"20px",marginRight:"5px", width:"150px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+          <img style={{marginTop:"20px",marginRight:"5px", width:"150px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+          <img style={{marginTop:"20px",marginRight:"5px", width:"150px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+          <img style={{marginTop:"20px",marginRight:"5px", width:"150px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+          <img style={{marginTop:"20px",marginRight:"5px", width:"150px"}} src="https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg?size=626&ext=jpg"/>
+
+          
+          
+          
+          
+          </GridItem>
+        </GridItem>
+
+
+        {/* CARACTERÍSTICAS */}
+        <GridItem xs={12} sm={12} md={12} lg={12} xl={12} style={{
+          display:"flex",
+          background:"cadetblue",
+    padding: "10px",
+    marginTop: "20px",
+    fontWeight: "700",
+    color: "white"
+
+          }}>
+
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.habitacion == 1 ? vivienda.habitacion+' Habitación':vivienda.habitacion+' Habitaciones'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.planta + 'Planta'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.banos == 1 ? vivienda.banos+' Baño': vivienda.banos+ 'Baños'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.ascensor ? 'Con Ascensor': 'Sin Ascensor'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.garaje ? 'Con Garaje': 'Sin Garaje'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.terraza ? 'Con Terraza': 'Sin Terraza'}</GridItem>
+          <GridItem xs={4} sm={4} md={4} lg={4} xl={4}>{vivienda.m2+'/m2'}</GridItem>
+        </GridItem>
+
+        {/* PRECIO Y CONTACTO */}
+
+        <GridItem xs={12} sm={12} md={12} lg={12} xl={12} style={{
+          
+          display:"flex",
+          justifyContent:"center",
+    padding: "15px",
+    justifyContent: "center",
+    background: "aliceblue",
+          
+          
+          
+          }}>
+        <GridItem xs={8} sm={8} md={8} lg={8} xl={8} style={{fontSize:"24px"}}>{vivienda.precio}€</GridItem>
+          <GridItem xs={2} sm={2} md={2} lg={2} xl={2}>LLAMAR</GridItem>
+          <GridItem xs={2} sm={2} md={2} lg={2} xl={2}>ENVIAR CORREO</GridItem>
+        </GridItem>
+
+
+        {/* DESCRIPCIÓN */}
+        <GridItem xs={12} sm={12} md={12} lg={12} xl={12} style={{marginTop:"15px", marginBottom:"15px"}}>
+          {vivienda.descripcion}
+          </GridItem>        
+
+      </GridContainer>
     )
+
 };
 
 
